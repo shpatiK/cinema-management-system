9becd5bd5d03f49e1eda9f3771786fd51c5b780b
eniguhelli <eniguhelli99@gmail.com>
2025-05-30 03:11:04 +0200

    Perditesimi i api.ts, per qellime te Deployment

--- cinema-frontend/src/services/api.ts
+++ cinema-frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "http://localhost:3000"
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3000";
 
 // Create axios instance with default config
 const api = axios.create({
